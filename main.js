cd5045d85b60b103541d1e6e6078ed578d126610
Ruth Gough <ruthegough5@gmail.com>
2021-09-16 00:08:36 -0500

    Adding code to pages. Drafting main.js elements, refining page directory.

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
-class NavInABiscuit extends HTMLElement {
-    this.innerHTML=
-
-    <header>
-        {/* <!-- Nav Bar by Santosh Goswami via codepen with my edits --> */
+class NavSticky extends HTMLElement {
+    connectedCallback(
+        this.innerHTML = `
+        <header>
         <a href="index.html">
-        <img src+#>
-    </a>
-    <script type=module src=main.js></script>
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Classes</a></li>
-        <li><a href="#">Events</a></li>
-        <li><a href="#">Membership</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
-</header>}
-        
+            <img src+#>
+        </a>
+        <script type=module src=main.js></script>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Classes</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Membership</a></li>
+        </ul>
+    </header>
+        `
+    )
 }
+customElements.define(`FooterSticky`, FooterSticky)
+class FooterSticky extends HTMLElement {
+    connectedCallback(
+        this.innerHTML = `
+ 
+<footer>
+<div>
+    <h5>Contact Us!</h5><label>Email:</label>
+            <a href="mailto: ruthegough5@gmail.com" ?subject="Mail" from RegionalArtsAlliancesOfKentucky .com
+                target="_blank">RegionalArtsAlliancesOfKentucky
+                .com</a>
+                <h5>
+                    ©2021 L.PulverizaCreative LLC_tbd
+                </h5>
+        </div>
+    </body>
+
+        `
+    )
+}
+customElements.define(`FooterSticky`, FooterSticky)
