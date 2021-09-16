1feba983651a7d12fa9c2b5e2ac5ecf5fee502c7
Ruth Gough <ruthegough5@gmail.com>
2021-09-15 22:54:03 -0500

    Draft-creation of framework/outline of LandingPage/Site Regional Arts Alliance of Kentucky©: index.html style.css About.html Classes.html Events.html Membership.html, main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+class NavInABiscuit extends HTMLElement {
+    this.innerHTML=
+
+    <header>
+        {/* <!-- Nav Bar by Santosh Goswami via codepen with my edits --> */
+        <a href="index.html">
+        <img src+#>
+    </a>
+    <script type=module src=main.js></script>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Classes</a></li>
+        <li><a href="#">Events</a></li>
+        <li><a href="#">Membership</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</header>}
+        
+}
